2acca619f8ce54b356b151a8a4576efb65c7ff3e
hdviettt <viethd2704@gmail.com>
2025-10-26 02:16:55 +0700

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="h-full overflow-y-auto">
+    <div className="h-full">
       <div className="min-h-full px-8 md:px-16 lg:px-24 py-8 md:py-12 lg:py-16 animate-fadeIn">
         <div className="max-w-2xl mx-auto space-y-12">
           {/* Main Profile Section */}
